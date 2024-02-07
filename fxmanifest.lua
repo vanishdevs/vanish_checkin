4dcacdf2423a54fbf6bacf4dde4039353fb7484f
Vanish <121704071+2zVanish@users.noreply.github.com>
2024-02-07 18:41:31 -0500

    add: ox lib

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,7 @@ description 'Simple ESX Check-in system to revive players'
 client_script 'client.lua'
 server_script 'server.lua'
 shared_scripts {
+    '@ox_lib/init.lua',
     '@es_extended/imports.lua',
     'config.lua'
 }
