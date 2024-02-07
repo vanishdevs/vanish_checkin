e1765b2e679fb4296835b45171d52d96fec23377
Vanish <121704071+2zVanish@users.noreply.github.com>
2024-02-07 18:39:41 -0500

    feat(add): version check [cred - Scully]

--- server.lua
+++ server.lua
@@ -1,3 +1,23 @@
+local function CheckVersion()
+    PerformHttpRequest('https://github.com/2zVanish/vanish-checkin/blob/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then
+            lib.print.error('Currently unable to run a version check.')
+            return
+        end
+
+        lib.print.info(('Current Version: %s'):format(currentVersion))
+        lib.print.info(('Latest Version: %s'):format(text))
+
+        if text == currentVersion then
+            lib.print.info('You are running the latest version.')
+        else
+            lib.print.error(('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
 RegisterServerEvent('vanishdev:recievetreatment')
 AddEventHandler('vanishdev:recievetreatment', function(price)
     local _source = source
@@ -24,3 +44,5 @@ AddEventHandler('vanishdev:recievetreatment', function(price)
         end
     end
 end)
+
+CheckVersion()
