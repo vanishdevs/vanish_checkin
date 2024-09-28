943b5e2594135dc11d71bca78179889b0ad70b23
vanishdevs <22zVanish@gmail.com>
2024-09-27 23:05:47 -0400

    fix(fxmanifest): Define client & server scripts properly... lol

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,8 @@ author 'vanishdev'
 description 'Check-in system to revive players'
 version '1.1.0'
 
-client_scripts 'client/*.lua'
-server_scripts 'server/*.lua'
+client_scripts { 'client/*.lua' }
+server_scripts { 'server/*.lua' }
 shared_scripts { '@ox_lib/init.lua', '@es_extended/imports.lua', 'config.lua' }
 files { 'locales/*.json' }
 
